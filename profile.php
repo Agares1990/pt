4e41754cde5fc59d58a5f249fd69179c23e3cac5
Agares1990 <lamjarad@gmail.com>
2022-03-27 17:27:33 +0200

    annuler une réservation : requête pour Afficher les réservation d'un client; requête pour supprimer une réservation; requête pour actualiser les réservation après suppression d'une réservation

--- profile.php
+++ profile.php
@@ -7,19 +7,36 @@ $css = "styleProfile";
 $pdo = getPDO();
 
 $email = $_SESSION['email'];
+$idReservation = $_POST['idReservation'];
 
+// Récupérer le prenom du client pour afficher un message de Bienvenue
 $client = $pdo->query("SELECT prenom FROM client WHERE email =  '$email'")->fetch();
 //$client = $client->fetch();
 
-
+// Afficher les réservation d'un client donné
 $reservations = $pdo->query("SELECT * FROM reservation_chambre
                   LEFT JOIN nom_categorie_chambre ON reservation_chambre.categorieChambreId = nom_categorie_chambre.categorieChambreId
                   LEFT JOIN client ON reservation_chambre.clientId = client.idClient
                   WHERE email = '$email' && langueId = '$lang'");
+if(isset($_POST['delete'])){
+
+  //Requête pour supprimer une réservation
+  $annulerReservation = $pdo->query("DELETE FROM reservation_chambre
+                  WHERE clientId IN(SELECT clientId FROM (SELECT * FROM reservation_chambre) AS reserv INNER JOIN client ON reservation_chambre.clientId = client.idClient
+                  WHERE email = '$email' && idReservationChambre = '$idReservation')");
+
+// Actualiser la page après suppression d'une réservation
+  $reservations = $pdo->query("SELECT * FROM reservation_chambre
+                    LEFT JOIN nom_categorie_chambre ON reservation_chambre.categorieChambreId = nom_categorie_chambre.categorieChambreId
+                    LEFT JOIN client ON reservation_chambre.clientId = client.idClient
+                    WHERE email = '$email' && langueId = '$lang'");
+  $messageSucces = "Votre réservation a bien été annuler";
+}
 
 echo $twig->render('profile.html.twig',
   	  array('css' => $css,
             'reservations'=> $reservations,
-            'client' => $client
+            'client' => $client,
+            'messageSucces' => $messageSucces
   				));
 ?>
