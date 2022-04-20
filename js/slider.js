298da95474ed6f87402388208603424433befefd
Agares1990 <lamjarad@gmail.com>
2022-04-20 16:34:41 +0200

    Fonction Imprimer récap réservation

--- js/slider.js
+++ js/slider.js
@@ -18,22 +18,3 @@ function chnageSlide() { // changer le slide automatiquement
 
 }
 setInterval(chnageSlide, 4000); // changer le slide chaque 4s
-
-
-
-// function chnageSlide2() { // changer le slide automatiquement
-//
-//   // On fait disparaitre le slide actuel
-//   $(".roomPhoto" + numero).fadeOut(duree, function()
-//   {
-//   // On fait apparaitre le slide suivant (numero + 1)
-//   numero = numero + 1;
-//   // Si le numéro est supérieur au nombre de slides alors le numéro est égal à 1
-//   if (numero > nombreSlides) {
-//       numero = 1;
-//   }
-//   $(".roomPhoto" + numero).fadeIn(duree);
-// });
-//
-// }
-// setInterval(chnageSlide2, 4000); // changer le slide chaque 4s
